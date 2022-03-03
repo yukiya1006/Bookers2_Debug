95256331f27fd2b9d0c338345416d8f27dddfdfe
yukiya1006 <96877368+yukiya1006@users.noreply.github.com>
2022-03-03 02:45:46 +0000

    bookers2のデバック

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,19 +10,6 @@ class UsersController < ApplicationController
     #フォローに関して
     @following_users = @user.following_user #@userがフォローしている人達
     @follower_users = @user.follower_user #@userをフォローしている人達
-
-    #過去７日分
-    @today_book = @books.created_today
-    @yesterday_book = @books.created_yesterday
-    @two_days_ago_book = @books.created_two_days_ago
-    @three_days_ago_book = @books.created_three_days_ago
-    @four_days_ago_book = @books.created_four_days_ago
-    @five_days_ago_book = @books.created_five_days_ago
-    @six_days_ago_book = @books.created_six_days_ago
-
-    #過去１週間分
-    @this_week_book = @books.created_this_week
-    @last_week_book = @books.created_last_week
   end
 
   def index
@@ -59,19 +46,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @users = @user.follower_user.all
   end
-  
-  #非同期での検索
-  def search
-  @user = User.find(params[:user_id])
-  @books = @user.books 
-  @book = Book.new
-  if params[:created_at] == ""
-    @search_book = "日付を選択してください"
-  else
-    create_at = params[:created_at]
-    @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
-  end
- 
+
   private
 
   def user_params
@@ -83,6 +58,5 @@ class UsersController < ApplicationController
     unless @user == current_user
       redirect_to user_path(current_user)
     end
-  end
   end
 end
